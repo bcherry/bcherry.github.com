52a4676a9693f19e92786214f942b604e85acfa3
Ben Cherry <bcherry@gmail.com>
2013-03-02 17:47:14 -0800

    build script

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,7 @@ echo "Building with Jekyll..."
 jekyll
 
 echo "Packaging and deploying to GitHub master..."
-echo "deploy `date`" | git commit-tree source^{tree}:_site -p $(cat .git/refs/heads/master)
-git update-ref refs/heads/master COMMIT_ID
-git checkout master
+git update-ref refs/heads/master $(echo 'Deploy `date`' | git commit-tree source^{tree}:_site -p $(cat .git/refs/heads/master))
 git push -f origin master
 
 git checkout source
